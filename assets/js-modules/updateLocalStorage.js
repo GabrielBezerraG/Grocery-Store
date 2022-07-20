ad7f4c2d71ce1b23337b89df79c224a6a55691de
Gabriel Bezerra <104681677+GabrielBezerraG@users.noreply.github.com>
2022-07-20 19:50:52 -0300

    Delete assets/js-modules directory

--- assets/js-modules/updateLocalStorage.js
+++
@@ -1,5 +0,0 @@
-import {shoppingCartArray} from './shoppingCartArray.js'
-
-export default function updateLocalStorage() {
-   localStorage.setItem('shoppingCart', JSON.stringify(shoppingCartArray));
-}
